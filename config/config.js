ad4f8ee25e60fa25a378ca45a1a943c0b8d1fa56
alvian <alvian.ardhiansyah@gmail.com>
2026-02-11 10:59:06 +0700

    update docker

--- config/config.js
+++ config/config.js
@@ -12,13 +12,6 @@ module.exports = {
     host: process.env.MYSQL_HOST,
     dialect: "mysql",
   },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
   production: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASS,
